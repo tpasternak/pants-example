59878da72b75cc52221b2ff9ec17aa328c9ca1c0
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-04-14 11:13:59 +0200

    rename

--- java_app/com/com/com1/HelloWorld.java
+++ java_app/com/com/com1/HelloWorld.java
@@ -1,8 +1,6 @@
 package com.com1;
 
-public class HelloWorld {
-
-    public static void main(String[] args) {
+    public class HelloWorld { public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         for (String msg: Hellos.hellos()) {
             System.out.println(msg);
