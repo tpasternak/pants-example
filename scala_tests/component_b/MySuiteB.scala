1a0993306cd66668788c1d194fcddc1b20009e56
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-04-30 16:53:07 +0200

    Three modules

---
+++ scala_tests/component_b/MySuiteB.scala
@@ -0,0 +1,10 @@
+package com
+import org.junit.runner.RunWith
+import org.scalatest.TestSuite
+import org.scalatestplus.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class MySuite extends TestSuite {
+    println("Auto-test!")
+  }
