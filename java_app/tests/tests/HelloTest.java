e6adb99e74191359e27974ff23753ae656c06970
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-05-27 18:25:24 +0200

    working

--- java_app/tests/tests/HelloTest.java
+++ java_app/tests/tests/HelloTest.java
@@ -1,5 +1,13 @@
 package tests;
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 /* Ensure our greetings are polite */
 public class HelloTest {
@@ -10,5 +18,17 @@ public class HelloTest {
 
       String value2 = System.getProperty("name2");
       assertEquals("namonetonete23", value2);
+
+
+      List<Integer> numbers = Lists.newArrayList(1, 2, 3, 6, 10, 34, 57, 89);
+      Function<Integer, Boolean> acceptEven = new Function<Integer, Boolean>() {
+          @Override
+          public Boolean apply(Integer number) {
+              return (number % 2) == 0;
+          }
+      };
+      //List<Integer> evenNumbers = Lists.newArrayList(Collections2.filter(numbers, acceptEven));
+      //Integer found = Collections.binarySearch(evenNumbers, 57);
+
   }
 }
--- java_format_lib/com/com1/Format.java
+++ java_format_lib/com/com1/Format.java
@@ -1,10 +1,10 @@
 package com.com1;
 
 import java.util.List;
-import java.util.Arrays;
 
 public class Format {
     public static String format(String text) {
         return "=== " + text + " ===";
+
     };
 }
