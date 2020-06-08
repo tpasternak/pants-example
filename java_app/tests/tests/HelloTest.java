207d4c9435b5d99db8d0c937b8301c4f5cebf794
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-06-08 16:22:55 +0200

    avbcc

--- java_app/tests/tests/HelloTest.java
+++ java_app/tests/tests/HelloTest.java
@@ -1,15 +1,14 @@
 package tests;
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
-import java.util.Collections;
+import java.io.Console;
 import java.util.List;
 
+
 import static org.junit.Assert.*;
-/* Ensure our greetings are polite */
+// Dummy comment
 public class HelloTest {
   @Test
   public void helloTest() {
@@ -19,16 +18,20 @@ public class HelloTest {
       String value2 = System.getProperty("name2");
       assertEquals("namonetonete23", value2);
 
-
       List<Integer> numbers = Lists.newArrayList(1, 2, 3, 6, 10, 34, 57, 89);
-      Function<Integer, Boolean> acceptEven = new Function<Integer, Boolean>() {
+      Function<Integer, Boolean> acceptEven = new Function<>() {
           @Override
           public Boolean apply(Integer number) {
               return (number % 2) == 0;
           }
       };
-      //List<Integer> evenNumbers = Lists.newArrayList(Collections2.filter(numbers, acceptEven));
-      //Integer found = Collections.binarySearch(evenNumbers, 57);
-
+      Runnable c = new Runnable() {
+          @Override
+          public void run() {
+              System.out.println("ss");
+          }
+      };
+      // List<Integer> evenNumbers = Lists.newArrayList(Collections2.filter(numbers, acceptEven));
+      // Integer found = Collections.binarySearch(evenNumbers, 57);
   }
 }
