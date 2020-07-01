85f06f805cbaa4b190656b53d657cd7150b534aa
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-07-01 18:11:37 +0200

    Adapt to new pants

--- java_app/tests/tests/HelloTest.java
+++ java_app/tests/tests/HelloTest.java
@@ -18,20 +18,5 @@ public class HelloTest {
       String value2 = System.getProperty("name2");
       assertEquals("namonetonete23", value2);
 
-      List<Integer> numbers = Lists.newArrayList(1, 2, 3, 6, 10, 34, 57, 89);
-      Function<Integer, Boolean> acceptEven = new Function<>() {
-          @Override
-          public Boolean apply(Integer number) {
-              return (number % 2) == 0;
-          }
-      };
-      Runnable c = new Runnable() {
-          @Override
-          public void run() {
-              System.out.println("ss");
-          }
-      };
-      // List<Integer> evenNumbers = Lists.newArrayList(Collections2.filter(numbers, acceptEven));
-      // Integer found = Collections.binarySearch(evenNumbers, 57);
   }
 }
